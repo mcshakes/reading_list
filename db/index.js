27ff0de1f8b4957286bfee4c989627c1d24b9ea7
mcshakes <efmccarthy26@gmail.com>
2021-05-13 08:01:22 -0600

    Playing with configuration on BD

--- db/index.js
+++ db/index.js
@@ -1,9 +1,16 @@
 const { Pool } = require("pg")
 
-const pool = new Pool()
+
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    port: 5432,
+    password: null
+})
 
 pool.on('connect', () => {
-    console.log('Connected to the DB');
+    console.log(`Connected to the DB: ${process.env.NODE_ENV}`);
 });
 
 module.exports = {
