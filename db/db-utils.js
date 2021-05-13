4224ace2d98b633740133655a1b5b28ce78237d3
mcshakes <efmccarthy26@gmail.com>
2021-05-13 13:39:13 -0600

    Figured out the error finally, and can test the DB utils.

--- db/db-utils.js
+++ db/db-utils.js
@@ -1,31 +1,32 @@
-const { Pool } = require('pg')
+const db = require("./index");
+// const { Pool } = require('pg')
 
-const getConnection = () => {
-    return {
-        user: process.env.PGUSER,
-        host: process.env.PGHOST,
-        database: process.env.PGDATABASE,
-        port: 5432,
-        password: null
-    }
-}
+// const getConnection = () => {
+//     return {
+//         user: process.env.PGUSER,
+//         host: process.env.PGHOST,
+//         database: process.env.PGDATABASE,
+//         port: 5432,
+//         password: null
+//     }
+// }
 
-const createTable = async function (tableName) => {
-    const pool = new Pool(getConnection)
+const createTable = async function (tableName) {
+    // const pool = new Pool(getConnection)
 
-    await pool.connect()
+    // const client = await db.connect()
 
-    return await pool.query(
+    return await db.query(
         `DROP TABLE IF EXISTS ${tableName};
             CREATE TABLE ${tableName} (
                 id SERIAL PRIMARY KEY, 
                 name VARCHAR(50) not null,
-                list_type VARCHAR(50) not null,
+                list_type VARCHAR(50) not null
                 );
         `
         )
     
-    await pool.end()
+    await db.release()
     return res
 }
 
@@ -36,3 +37,17 @@ const createTable = async function (tableName) => {
 // created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
 // completed boolean DEFAULT FALSE
 // reading_list_id INTEGER REFERENCES reading_lists (id) on DELETE CASCADE);
+
+const dropTable = async function (tableName) {
+    // const client = new Pool(getConnection())
+
+    // const client = await db.connect()
+
+    await db.query(`DROP TABLE IF EXISTS ${tableName}`)
+    await db.end().then(() => console.log("Test Pool has ended"))
+}
+
+module.exports = {
+    createTable,
+    dropTable
+}
---
+++ tests/poop.js
@@ -0,0 +1,10 @@
+// const { createTable, dropTable } = require("../db/db-utils");
+// const supertest = require("supertest");
+// const app = require("../app");
+
+// const api = supertest(app)
+
+// describe("Reading List Service", () => {
+
+// })
+
